a39cddc9107c58e88fd1c4b6e831e966be8a6221
Fdobrovols <78583045+Fdobrovols@users.noreply.github.com>
2023-09-11 22:41:46 +0300

    fix

--- src/Components/PostItem.jsx
+++ src/Components/PostItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import {  View, Image, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
 
